e4a6a11de8f5535449a8737899837c0bc85a044b
Ahmad mehtab <ahmad.mhtb@gmail.com>
2025-05-26 21:57:58 +0500

    added some change

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // stript file added
+// this comment is used for difference between two branches
+// third line for pull request
+// this for pull (PR)
+// this is for conflic, how conflict occure when changes done in two branches
