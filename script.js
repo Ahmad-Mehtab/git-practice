e6def52c92a21aa308a8c898ee5e00694e869e64
Ahmad mehtab <ahmad.mhtb@gmail.com>
2025-05-26 20:37:06 +0500

    added conflict line

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 // this comment is used for difference between two branches
 // third line for pull request
 // this for pull (PR)
+// this is for conflic, how conflict occure when changes done in two branches
